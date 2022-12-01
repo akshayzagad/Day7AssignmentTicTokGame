bc41f79b80a1d509d274a038348c441fe39068b4
akshayzagad <akshayzagade62@gmail.com.com>
2022-12-01 06:42:33 +0530

    Added: Display empty board on screen

--- src/com/bridglabz/TicTokGame.java
+++ src/com/bridglabz/TicTokGame.java
@@ -3,5 +3,16 @@ package com.bridglabz;
 public class TicTokGame {
     public static void main(String[] args) {
         System.out.println("Welcome To TIC TOC Board Game");
-    }
+            char[][] gameBoard = {{'_', '|', '_', '|', '_'}, {'_', '|', '_', '|', '_'}, {'_', '|', '_', '|', '_'}};
+            printBOARD(gameBoard);
+        }
+
+        public static void printBOARD(char [][] gameBoard){
+            for (int i = 0; i < gameBoard.length; i++) {
+                for (int j = 0; j < gameBoard[i].length; j++) {
+                    System.out.print(gameBoard[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
 }
